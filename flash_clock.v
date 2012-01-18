efe3ca88776934d823e6c06c72a19d7a34e355ab
Krystian Wojtas <krystian.wojtas@gmail.com>
2012-01-19 00:28:42 +0100

    bad simulation

--- flash_clock.v
+++ flash_clock.v
@@ -20,24 +20,27 @@
 //////////////////////////////////////////////////////////////////////////////////
 module flash_clock //rename FlashTimer ??
 	(input CLK_50MHZ,
-	 inout reg ft_action
+	 inout ft_action
 	);
 	
-reg [3:0] cnt; //TODO czasy
+	reg [3:0] cnt; //TODO czasy
+	reg ft_action_;
 
-always @(posedge ft_action)
-begin
-	cnt = 0;
-end
-	
-always @(posedge CLK_50MHZ)
-	if(ft_action) begin //TODO czy przejdzie? ft_action rowniez w module wyzej
-		if(cnt<4'd7) cnt<=cnt+1;
-		else
-		begin
-			cnt<=0;
-			ft_action<=0;
-		end
+	always @(posedge ft_action_)
+	begin
+		cnt = 0;
 	end
+		
+	always @(posedge CLK_50MHZ)
+		if(ft_action_) begin //TODO czy przejdzie? ft_action rowniez w module wyzej
+			if(cnt<4'd7) cnt<=cnt+1;
+			else
+			begin
+				cnt<=0;
+				ft_action_<=0;
+			end
+		end
+
+	assign ft_action = ft_action_;
 
 endmodule
--- flash_test.v
+++ flash_test.v
@@ -18,32 +18,42 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module flash_test();
+module flash_test;
 	 
 	//output czasomierz_start,
 	//input czasomierz_done	
 	reg CLK_50MHZ;
 	reg RST;	
 	//linie flasha
-	//reg NF_CE, NF_BYTE, NF_OE, NF_RP, NF_WE, NF_WP, NF_STS;
-	reg NF_CE;
-	reg NF_BYTE;
-	reg NF_OE;
-	reg NF_RP;
-	reg NF_WE;
-	reg NF_WP;
-	reg NF_BYTE;
+	wire NF_CE, NF_BYTE, NF_OE, NF_RP, NF_WE, NF_WP;
 	reg NF_STS;
-	reg NF_A[7:0];
-	reg NF_D[7:0];
+//	reg NF_CE;
+//	reg NF_BYTE;
+//	reg NF_OE;
+//	reg NF_RP;
+//	reg NF_WE;
+//	reg NF_WP;
+//	reg NF_STS;
+	wire [7:0] NF_A;
+	wire [7:0] NF_D;
 	//interfejs modulu
-	reg addr[7:0];
-	reg data[7:0];
-	wire direction_rw;
+	wire [7:0] addr;
+	wire [7:0] data;
+	reg direction_rw;
 	wire fb_action;
 	//czasomierz flasha
 	wire ft_action;
 	
+	reg [7:0] NF_A_;
+	reg [7:0] NF_D_;
+	reg [7:0] addr_;
+	reg [7:0] data_;
+	reg fb_action_;
+	assign NF_A = NF_A_;
+	assign NF_D = NF_D_;
+	assign addr = addr_;
+	assign data = data_;
+	assign fb_action = fb_action_;	
 	
 	flash_clock fl_c(CLK_50MHZ, ft_action);
 	flash fl(RST, CLK_50MHZ, NF_CE, NF_BYTE, NF_OE, NF_RP, NF_WE, NF_WP, NF_STS, NF_A[7:0], NF_D[7:0], addr[7:0], data[7:0], direction_rw, fb_action, ft_action);
@@ -52,14 +62,21 @@ module flash_test();
 	initial
 	begin	
 		#20;
-		NF_A[7:0] = 8'b11111000;
-		NF_D[7:0] = 8'b00011111;
+		NF_A_[7:0] = 8'b11111000;
+		NF_D_[7:0] = 8'b00011111;
 		#40;
-		addr[7:0] = 8'b00110101;
-		data[7:0] = 8'b11001001;
+		addr_[7:0] = 8'b00110101;
+		data_[7:0] = 8'b11001001;
 		direction_rw = 1'b1; //write
 		#40;
-		fb_action = 1;
+		fb_action_ = 1;
 	end
+	
+	/*** SIMULATION CLK 50 MHZ ***/
+	always
+		begin
+		#1;
+		CLK_50MHZ = ~CLK_50MHZ;
+		end
 	
 endmodule
